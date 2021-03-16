2231eb3275786f4c2eb1588cd7305280c7c892a2
Andrew Walbran <qwandor@google.com>
2021-03-16 20:55:41 +0000

    Trying to get USART working with stm321fxx_hal crate.

--- hoverkite-firmware/src/main.rs
+++ hoverkite-firmware/src/main.rs
@@ -10,6 +10,55 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use core::fmt::Write;
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f1::stm32f103;
+use stm32f1xx_hal::{
+    pac,
+    prelude::*,
+    serial::{Config, Serial},
+};
 
 #[entry]
-fn main() -> ! {}
+fn main() -> ! {
+    // Get access to the core peripherals from the cortex-m crate
+    let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+
+    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+    // HAL structs
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+    // `clocks`
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // Prepare the alternate function I/O registers
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    // Prepare the GPIOA peripheral
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+
+    // USART2
+    let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+    let rx = gpioa.pa3;
+
+    // Set up the usart device. Takes ownership over the USART register and tx/rx pins. The rest of
+    // the registers are used to enable and configure the device.
+    let serial = Serial::usart2(
+        dp.USART2,
+        (tx, rx),
+        &mut afio.mapr,
+        Config::default().baudrate(115200.bps()),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    // Split the serial struct into a receiving and a transmitting part
+    let (mut tx, _rx) = serial.split();
+
+    loop {
+        // your code goes here
+        writeln!(tx, "Hello!").unwrap();
+    }
+}
