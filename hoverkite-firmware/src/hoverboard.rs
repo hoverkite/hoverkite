404d2e2da50921baadd056171943e7d0da768625
Andrew Walbran <qwandor@google.com>
2021-03-20 12:36:00 +0000

    Split out module with hoverboard peripherals.

---
+++ hoverkite-firmware/src/hoverboard.rs
@@ -0,0 +1,63 @@
+use stm32f0xx_hal::{
+    gpio::{
+        gpioa::{PA0, PA12, PA15, PA2, PA3},
+        gpiob::{PB10, PB3},
+        Alternate, Output, PushPull, AF1,
+    },
+    pac::{GPIOA, GPIOB, USART2},
+    prelude::*,
+    rcc::Rcc,
+    serial::Serial,
+};
+
+pub struct Hoverboard {
+    pub serial: Serial<USART2, PA2<Alternate<AF1>>, PA3<Alternate<AF1>>>,
+    pub side_led: PA0<Output<PushPull>>,
+    pub green_led: PA15<Output<PushPull>>,
+    pub orange_led: PA12<Output<PushPull>>,
+    pub red_led: PB3<Output<PushPull>>,
+    pub buzzer: PB10<Output<PushPull>>,
+}
+
+impl Hoverboard {
+    pub fn new(gpioa: GPIOA, gpiob: GPIOB, usart2: USART2, rcc: &mut Rcc) -> Hoverboard {
+        let gpioa = gpioa.split(rcc);
+        let gpiob = gpiob.split(rcc);
+
+        // NB: Don't try to use pa13, that's SWDIO
+        let pa0 = gpioa.pa0;
+        let pa15 = gpioa.pa15;
+        let pa12 = gpioa.pa12;
+        let pb3 = gpiob.pb3;
+        let (side_led, green_led, orange_led, red_led) = cortex_m::interrupt::free(|cs| {
+            (
+                pa0.into_push_pull_output(cs),
+                pa15.into_push_pull_output(cs),
+                pa12.into_push_pull_output(cs),
+                pb3.into_push_pull_output(cs),
+            )
+        });
+
+        let pb10 = gpiob.pb10;
+        let buzzer = cortex_m::interrupt::free(|cs| pb10.into_push_pull_output(cs));
+
+        // Prepare the alternate function I/O registers for the USART.
+        let pa2 = gpioa.pa2;
+        let pa3 = gpioa.pa3;
+        let (tx, rx) = cortex_m::interrupt::free(move |cs| {
+            (pa2.into_alternate_af1(cs), pa3.into_alternate_af1(cs))
+        });
+        // Set up the usart device. Takes ownership over the USART register and tx/rx pins. The rest of
+        // the registers are used to enable and configure the device.
+        let serial = Serial::usart2(usart2, (tx, rx), 115200.bps(), rcc);
+
+        Hoverboard {
+            serial,
+            side_led,
+            green_led,
+            orange_led,
+            red_led,
+            buzzer,
+        }
+    }
+}
--- hoverkite-firmware/src/main.rs
+++ hoverkite-firmware/src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod hoverboard;
+
+use hoverboard::Hoverboard;
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
                      // use panic_abort as _; // requires nightly
@@ -12,48 +15,20 @@ use core::str::from_utf8;
 use cortex_m_rt::entry;
 use embedded_hal::serial::Read;
 use nb::block;
-use stm32f0xx_hal::{pac, prelude::*, serial::Serial};
+use stm32f0xx_hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
-    // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH;
     let mut rcc = dp.RCC.configure().freeze(&mut flash);
 
-    let gpioa = dp.GPIOA.split(&mut rcc);
-    let gpiob = dp.GPIOB.split(&mut rcc);
-
-    // NB: Don't try to use pa13, that's SWDIO
-    let pa0 = gpioa.pa0;
-    let pa15 = gpioa.pa15;
-    let pa12 = gpioa.pa12;
-    let pb3 = gpiob.pb3;
-    let (mut side_led, mut green_led, mut orange_led, mut red_led) =
-        cortex_m::interrupt::free(|cs| {
-            (
-                pa0.into_push_pull_output(cs),
-                pa15.into_push_pull_output(cs),
-                pa12.into_push_pull_output(cs),
-                pb3.into_push_pull_output(cs),
-            )
-        });
-
-    // Prepare the alternate function I/O registers for the USART.
-    let pa2 = gpioa.pa2;
-    let pa3 = gpioa.pa3;
-    let (tx, rx) = cortex_m::interrupt::free(move |cs| {
-        (pa2.into_alternate_af1(cs), pa3.into_alternate_af1(cs))
-    });
-    // Set up the usart device. Takes ownership over the USART register and tx/rx pins. The rest of
-    // the registers are used to enable and configure the device.
-    let serial = Serial::usart2(dp.USART2, (tx, rx), 115200.bps(), &mut rcc);
+    let mut hoverboard = Hoverboard::new(dp.GPIOA, dp.GPIOB, dp.USART2, &mut rcc);
 
     // Split the serial struct into a receiving and a transmitting part
-    let (mut tx, mut rx) = serial.split();
+    let (mut tx, mut rx) = hoverboard.serial.split();
 
     let mut line = [0; 20];
     writeln!(tx, "Ready").unwrap();
@@ -65,44 +40,44 @@ fn main() -> ! {
             b'l' => match line[1] {
                 b'0' => {
                     writeln!(tx, "LED off").unwrap();
-                    side_led.set_low().unwrap()
+                    hoverboard.side_led.set_low().unwrap()
                 }
                 b'1' => {
                     writeln!(tx, "LED on").unwrap();
-                    side_led.set_high().unwrap()
+                    hoverboard.side_led.set_high().unwrap()
                 }
                 _ => writeln!(tx, "LED unrecognised").unwrap(),
             },
             b'o' => match line[1] {
                 b'0' => {
                     writeln!(tx, "orange off").unwrap();
-                    orange_led.set_low().unwrap()
+                    hoverboard.orange_led.set_low().unwrap()
                 }
                 b'1' => {
                     writeln!(tx, "orange on").unwrap();
-                    orange_led.set_high().unwrap()
+                    hoverboard.orange_led.set_high().unwrap()
                 }
                 _ => writeln!(tx, "LED unrecognised").unwrap(),
             },
             b'r' => match line[1] {
                 b'0' => {
                     writeln!(tx, "red off").unwrap();
-                    red_led.set_low().unwrap()
+                    hoverboard.red_led.set_low().unwrap()
                 }
                 b'1' => {
                     writeln!(tx, "red on").unwrap();
-                    red_led.set_high().unwrap()
+                    hoverboard.red_led.set_high().unwrap()
                 }
                 _ => writeln!(tx, "LED unrecognised").unwrap(),
             },
             b'g' => match line[1] {
                 b'0' => {
                     writeln!(tx, "green off").unwrap();
-                    green_led.set_low().unwrap()
+                    hoverboard.green_led.set_low().unwrap()
                 }
                 b'1' => {
                     writeln!(tx, "green on").unwrap();
-                    green_led.set_high().unwrap()
+                    hoverboard.green_led.set_high().unwrap()
                 }
                 _ => writeln!(tx, "LED unrecognised").unwrap(),
             },
