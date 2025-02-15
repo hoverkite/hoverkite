f43a70abb0ed4bb9eb6c85acf546a9e22c0e3d20
David Laban <alsuren@gmail.com>
2025-02-15 23:42:00 +0000

    esp-generate --chip=esp32 kitebox -o alloc -o wifi -o ble -o embassy -o vscode --headless

---
+++ cross/kitebox/src/bin/async_main.rs
@@ -0,0 +1,46 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::clock::CpuClock;
+use log::info;
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+
+extern crate alloc;
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    // generator version: 0.2.2
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(72 * 1024);
+
+    esp_println::logger::init_logger_from_env();
+
+    let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
+    esp_hal_embassy::init(timer0.timer0);
+
+    info!("Embassy initialized!");
+
+    let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
+    let _init = esp_wifi::init(
+        timer1.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+}
