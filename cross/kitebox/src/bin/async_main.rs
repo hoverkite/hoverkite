e03586ffb71961facf8fee68769352584a4b7a64
David Laban <alsuren@gmail.com>
2025-02-21 16:06:11 +0000

    WIP trying to get esp32 uart working. Works at 9600 but not 1M

--- cross/kitebox/src/bin/async_main.rs
+++ cross/kitebox/src/bin/async_main.rs
@@ -1,14 +1,65 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
+use esp_hal::{
+    uart::{Uart, UartRx, UartTx},
+    Async,
+};
 use log::info;
+use static_cell::StaticCell;
 
-use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 
 extern crate alloc;
+const READ_BUF_SIZE: usize = 64;
+
+#[embassy_executor::task]
+async fn writer(mut tx: UartTx<'static, Async>, signal: &'static Signal<NoopRawMutex, usize>) {
+    use core::fmt::Write;
+    embedded_io_async::Write::write(
+        &mut tx,
+        b"Hello async serial. Enter something ended with EOT (CTRL-D).\r\n",
+    )
+    .await
+    .unwrap();
+
+    info!("writer");
+
+    embedded_io_async::Write::flush(&mut tx).await.unwrap();
+    loop {
+        let bytes_read = signal.wait().await;
+        signal.reset();
+        write!(&mut tx, "\r\n-- received {} bytes --\r\n", bytes_read).unwrap();
+        embedded_io_async::Write::flush(&mut tx).await.unwrap();
+    }
+}
+
+#[embassy_executor::task]
+async fn reader(mut rx: UartRx<'static, Async>, signal: &'static Signal<NoopRawMutex, usize>) {
+    const MAX_BUFFER_SIZE: usize = 10 * READ_BUF_SIZE + 16;
+
+    let mut rbuf: [u8; MAX_BUFFER_SIZE] = [0u8; MAX_BUFFER_SIZE];
+    let mut offset = 0;
+
+    info!("reader");
+
+    loop {
+        let r = embedded_io_async::Read::read(&mut rx, &mut rbuf[offset..]).await;
+        match r {
+            Ok(len) => {
+                offset += len;
+                esp_println::println!("Read: {len}, data: {:?}", &rbuf[..offset]);
+                offset = 0;
+                signal.signal(len);
+            }
+            Err(e) => esp_println::println!("RX Error: {:?}", e),
+        }
+    }
+}
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -34,12 +85,36 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
+    // "UART0"
+    // let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
+    let config = esp_hal::uart::Config::default().with_baudrate(9600);
+    // .with_rx(RxConfig::default().with_fifo_full_threshold(READ_BUF_SIZE as u16));
+
+    let mut uart2 = Uart::new(peripherals.UART2, config)
+        .unwrap()
+        .with_tx(peripherals.GPIO19)
+        .with_rx(peripherals.GPIO22)
+        .into_async();
     // TODO: Spawn some tasks
     let _ = spawner;
 
+    let (rx, mut tx) = uart2.split();
+
+    static SIGNAL: StaticCell<Signal<NoopRawMutex, usize>> = StaticCell::new();
+    let signal = &*SIGNAL.init(Signal::new());
+
+    embedded_io_async::Write::write(
+        &mut tx,
+        b"Hello async serial. Enter something ended with EOT (CTRL-D).\r\n",
+    )
+    .await
+    .unwrap();
+
+    spawner.spawn(reader(rx, &signal)).ok();
+    spawner.spawn(writer(tx, &signal)).ok();
     loop {
         info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        Timer::after(Duration::from_secs(100)).await;
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
