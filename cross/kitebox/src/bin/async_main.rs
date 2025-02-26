80bbd51e68f04e8bdd339126790448dae0e81ac1
David Laban <alsuren@gmail.com>
2025-02-26 12:28:30 +0000

    no need for cfg_if; fix missing unwrap

--- cross/kitebox/src/bin/async_main.rs
+++ cross/kitebox/src/bin/async_main.rs
@@ -34,7 +34,7 @@ async fn reader(mut rx: UartRx<'static, Async>, mut tx: UartTx<'static, Async>) 
         &mut tx,
         b"Hello async serial. Enter something ended with EOT (CTRL-D).\r\n",
     )
-    .await;
+    .await.unwrap();
     loop {
         let r = embedded_io_async::Read::read(&mut rx, &mut rbuf[offset..]).await;
         match r {
@@ -60,22 +60,7 @@ async fn main(spawner: Spawner) {
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
 
-    // Default pins for Uart communication
-    cfg_if::cfg_if! {
-        if #[cfg(feature = "esp32")] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
-        } else if #[cfg(feature = "esp32c2")] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO20, peripherals.GPIO19);
-        } else if #[cfg(feature = "esp32c3")] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO21, peripherals.GPIO20);
-        } else if #[cfg(feature = "esp32c6")] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO16, peripherals.GPIO17);
-        } else if #[cfg(feature = "esp32h2")] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO24, peripherals.GPIO23);
-        } else if #[cfg(any(feature = "esp32s2", feature = "esp32s3"))] {
-            let (tx_pin, rx_pin) = (peripherals.GPIO43, peripherals.GPIO44);
-        }
-    }
+    let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
 
     let config = Config::default()
         .with_rx(RxConfig::default().with_fifo_full_threshold(READ_BUF_SIZE as u16));
