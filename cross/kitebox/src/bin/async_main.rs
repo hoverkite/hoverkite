ab128885c8ab644180165b07fcec0c41addd53a1
David Laban <alsuren@gmail.com>
2025-02-26 11:53:29 +0000

    start from scratch with slightly simplified embassy_serial.rs example

--- cross/kitebox/src/bin/async_main.rs
+++ cross/kitebox/src/bin/async_main.rs
@@ -1,122 +1,93 @@
+//! embassy serial
+//!
+//! This is an example of running the embassy executor and asynchronously
+//! writing to and reading from UART.
+
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//% FEATURES: embassy esp-hal/unstable
+
 #![no_std]
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, signal::Signal};
-use embedded_io::Write;
 use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
 use esp_hal::{
-    uart::{Uart, UartRx, UartTx},
+    timer::timg::TimerGroup,
+    uart::{AtCmdConfig, Config, RxConfig, Uart, UartRx, UartTx},
     Async,
 };
-use log::info;
-use static_cell::StaticCell;
 
-use embassy_time::{Duration, Timer, WithTimeout};
-
-extern crate alloc;
+// fifo_full_threshold (RX)
 const READ_BUF_SIZE: usize = 64;
+// EOT (CTRL-D)
+const AT_CMD: u8 = 0x04;
 
-// FIXME: just pass the whole uart in here?
 #[embassy_executor::task]
-async fn servo_controller(
-    mut rx: UartRx<'static, Async>,
-    mut tx: UartTx<'static, Async>,
-    signal: &'static Signal<NoopRawMutex, usize>,
-) {
-    use core::fmt::Write;
-    embedded_io_async::Write::write(
-        &mut tx,
-        b"Hello async serial. Enter something ended with EOT (CTRL-D).\r\n",
-    )
-    .await
-    .unwrap();
-
+async fn reader(mut rx: UartRx<'static, Async>, mut tx: UartTx<'static, Async>) {
     const MAX_BUFFER_SIZE: usize = 10 * READ_BUF_SIZE + 16;
 
     let mut rbuf: [u8; MAX_BUFFER_SIZE] = [0u8; MAX_BUFFER_SIZE];
     let mut offset = 0;
 
-    info!("servo controller task started");
-
-    embedded_io_async::Write::flush(&mut tx).await.unwrap();
-
+    use core::fmt::Write;
+    embedded_io_async::Write::write(
+        &mut tx,
+        b"Hello async serial. Enter something ended with EOT (CTRL-D).\r\n",
+    )
+    .await;
     loop {
-        const BROADCAST: u8 = 254; // 0xFE
-
-        // seems reasonable to do a blocking write here: if we only manage to send half a message
-        // over the uart then we're probably not going to get the servos doing what we want them to anyway?
-        tx.write_all(&[0xff, 0xff, BROADCAST, 0x02, 0x01, 0xfB])
-            .unwrap();
-        embedded_io_async::Write::flush(&mut tx).await.unwrap();
-
-        let r = embedded_io_async::Read::read(&mut rx, &mut rbuf[offset..])
-            .with_timeout(Duration::from_secs(1))
-            .await;
+        let r = embedded_io_async::Read::read(&mut rx, &mut rbuf[offset..]).await;
         match r {
-            Ok(Ok(len)) => {
+            Ok(len) => {
                 offset += len;
                 esp_println::println!("Read: {len}, data: {:?}", &rbuf[..offset]);
                 offset = 0;
-                signal.signal(len);
+
+                let bytes_read = len;
+                write!(&mut tx, "\r\n-- received {} bytes --\r\n", bytes_read).unwrap();
+                embedded_io_async::Write::flush(&mut tx).await.unwrap();
             }
-            Ok(Err(e)) => esp_println::println!("RX Error: {:?}", e),
-            Err(e) => esp_println::println!("read timeout: {:?}", e),
+            Err(e) => esp_println::println!("RX Error: {:?}", e),
         }
     }
 }
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.2.2
+    esp_println::println!("Init!");
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
 
-    esp_alloc::heap_allocator!(72 * 1024);
-
-    esp_println::logger::init_logger_from_env();
-
-    let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
-    esp_hal_embassy::init(timer0.timer0);
-
-    info!("Embassy initialized!");
-
-    let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
-        timer1.timer0,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
-
-    let config = esp_hal::uart::Config::default()
-        .with_baudrate(1_000_000)
-        // 8N1
-        .with_data_bits(esp_hal::uart::DataBits::_8)
-        .with_parity(esp_hal::uart::Parity::None)
-        .with_stop_bits(esp_hal::uart::StopBits::_1);
-
-    let uart1 = Uart::new(peripherals.UART1, config)
-        .unwrap()
-        .with_rx(peripherals.GPIO18)
-        .with_tx(peripherals.GPIO19)
-        .into_async();
-
-    let _ = spawner;
-
-    let (rx, tx) = uart1.split();
-
-    static SIGNAL: StaticCell<Signal<NoopRawMutex, usize>> = StaticCell::new();
-    let signal = &*SIGNAL.init(Signal::new());
-
-    spawner.spawn(servo_controller(rx, tx, &signal)).ok();
-
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(100)).await;
+    // Default pins for Uart communication
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "esp32")] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO1, peripherals.GPIO3);
+        } else if #[cfg(feature = "esp32c2")] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO20, peripherals.GPIO19);
+        } else if #[cfg(feature = "esp32c3")] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO21, peripherals.GPIO20);
+        } else if #[cfg(feature = "esp32c6")] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO16, peripherals.GPIO17);
+        } else if #[cfg(feature = "esp32h2")] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO24, peripherals.GPIO23);
+        } else if #[cfg(any(feature = "esp32s2", feature = "esp32s3"))] {
+            let (tx_pin, rx_pin) = (peripherals.GPIO43, peripherals.GPIO44);
+        }
     }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+    let config = Config::default()
+        .with_rx(RxConfig::default().with_fifo_full_threshold(READ_BUF_SIZE as u16));
+
+    let mut uart0 = Uart::new(peripherals.UART0, config)
+        .unwrap()
+        .with_tx(tx_pin)
+        .with_rx(rx_pin)
+        .into_async();
+    uart0.set_at_cmd(AtCmdConfig::default().with_cmd_char(AT_CMD));
+
+    let (rx, tx) = uart0.split();
+
+    spawner.spawn(reader(rx, tx)).ok();
 }
