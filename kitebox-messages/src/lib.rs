77875b5f1bfb05b7ce522e2c7c6812ab25e530fa
David Laban <alsuren@gmail.com>
2025-03-20 14:08:27 +0000

    use capnp_conv to simplify things a bit

--- kitebox-messages/src/lib.rs
+++ kitebox-messages/src/lib.rs
@@ -1,7 +1,45 @@
 #![no_std]
 
+use capnp_conv::capnp_conv;
+
 pub mod kitebox_messages_capnp {
     include!(concat!(env!("OUT_DIR"), "/kitebox_messages_capnp.rs"));
+}
+
+#[capnp_conv(kitebox_messages_capnp::axis_data)]
+#[derive(Debug, PartialEq, Eq)]
+pub struct AxisData {
+    x: i16,
+    y: i16,
+    z: i16,
+}
+
+#[capnp_conv(kitebox_messages_capnp::data)]
+#[derive(Debug, PartialEq, Eq)]
+pub struct Data {
+    acc: AxisData,
+    gyr: AxisData,
+    time: u32,
+}
+
+#[capnp_conv(kitebox_messages_capnp::time)]
+#[derive(Debug, PartialEq, Eq)]
+struct Time {
+    time: u32,
+}
+
+#[capnp_conv(kitebox_messages_capnp::message::body)]
+#[derive(Debug, PartialEq, Eq)]
+enum Body {
+    Data(Data),
+    Time(Time),
+}
+
+#[capnp_conv(kitebox_messages_capnp::message)]
+#[derive(Debug, PartialEq, Eq)]
+struct Message {
+    #[capnp_conv(type = "union")]
+    body: Body,
 }
 
 // FIXME: if I know that I don't have any arrays in my structs, is there a way to get capnp
@@ -16,6 +54,7 @@ mod tests {
     use super::*;
     use capnp::message::SingleSegmentAllocator;
     use capnp::traits::HasStructSize;
+    use capnp_conv::{Readable, Writable};
 
     #[test]
     fn test_segment_allocator_size_is_big_enough() {
@@ -34,21 +73,19 @@ mod tests {
         let mut segment = [0; SEGMENT_ALLOCATOR_SIZE];
         let mut message = capnp::message::Builder::new(SingleSegmentAllocator::new(&mut segment));
 
-        {
-            let root: kitebox_messages_capnp::message::Builder = message.init_root();
-            let mut data = root.init_body().init_data();
-            data.set_time(1000);
+        let data = Message {
+            body: Body::Data(Data {
+                acc: AxisData { x: 1, y: -1, z: 0 },
+                gyr: AxisData {
+                    x: 10,
+                    y: -10,
+                    z: 0,
+                },
+                time: 1000,
+            }),
+        };
 
-            let mut acc = data.reborrow().init_acc();
-            acc.set_x(1);
-            acc.set_y(-1);
-            acc.set_z(0);
-
-            let mut gyr = data.reborrow().init_gyr();
-            gyr.set_x(10);
-            gyr.set_y(-10);
-            gyr.set_z(0);
-        }
+        data.write(message.init_root());
 
         let segments = message.get_segments_for_output();
 
@@ -60,19 +97,8 @@ mod tests {
             .get_root::<kitebox_messages_capnp::message::Reader>()
             .unwrap();
 
-        let data = match root.get_body().which().unwrap() {
-            kitebox_messages_capnp::message::body::Which::Data(data) => data.unwrap(),
-            kitebox_messages_capnp::message::body::Which::Time(_) => panic!("expected data"),
-        };
+        let round_tripped = Message::read(root).unwrap();
 
-        assert_eq!(data.get_time(), 1000);
-
-        assert_eq!(data.get_acc().unwrap().get_x(), 1);
-        assert_eq!(data.get_acc().unwrap().get_y(), -1);
-        assert_eq!(data.get_acc().unwrap().get_z(), 0);
-
-        assert_eq!(data.get_gyr().unwrap().get_x(), 10);
-        assert_eq!(data.get_gyr().unwrap().get_y(), -10);
-        assert_eq!(data.get_gyr().unwrap().get_z(), 0);
+        assert_eq!(round_tripped, data);
     }
 }
